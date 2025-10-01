c00bb00e36abb8b8061d46c40acf90af021958ec
Alexei Kozhushkov <alexei.kozhushkov@gmail.com>
2025-10-01 15:08:10 +0300

    monorepo

--- sst.config.ts
+++ sst.config.ts
@@ -1,18 +1,17 @@
 /// <reference path="./.sst/platform/config.d.ts" />
-
 export default $config({
   app(input) {
     return {
-      name: "monorepo-template",
+      name: "optimic-ai",
       removal: input?.stage === "production" ? "retain" : "remove",
       protect: ["production"].includes(input?.stage),
       home: "aws",
+      providers: { aws: "7.7.0", random: "4.18.3" },
     };
   },
   async run() {
     const storage = await import("./infra/storage");
     await import("./infra/api");
-
     return {
       MyBucket: storage.bucket.name,
     };
